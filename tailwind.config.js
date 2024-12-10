3ea7ec2b6e8967612e198702c886db1b9d5b7797
Muhamad Ali Ridho <muhamadaliridho01@gmail.com>
2024-12-10 21:12:54 +0700

    2 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography"; // Ganti require dengan import
+
 export default {
   darkMode: "media",
   content: [
@@ -12,20 +14,10 @@ export default {
         lg: "3px 3px 6px rgba(0, 0, 0, 0.6)",
         xl: "4px 4px 8px rgba(0, 0, 0, 0.7)",
       },
-      // Adding the keyframes and animations for the slide-left animation
-      keyframes: {
-        "slide-left": {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-      },
-      animation: {
-        "slide-left-infinite": "slide-left 20s linear infinite",
-      },
     },
   },
   plugins: [
-    require("@tailwindcss/typography"),
+    typography, // Ganti require dengan variabel import
     function ({ addUtilities }) {
       addUtilities({
         ".text-shadow-sm": {
